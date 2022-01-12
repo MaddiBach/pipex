2f10267150e8b68e5b849ff1fa58e98b78cd2579
Maddibach <42.maddi@gmail.com>
2022-01-12 17:16:23 +0100

    + lstaddback +lstiter

--- lists.c
+++ lists.c
@@ -12,17 +12,42 @@
 
 #include    "pipex.h"
 
-t_cmd   *ft_makelst(char **envp, char **av)
+t_cmd   *ft_newlst(char **envp, char *arg, int index)
 {
     t_cmd   *new;
 
     new = malloc(sizeof(t_cmd));
     if (!new)
         return (NULL);
-    new->args = ft_get_args("ls -la -l -rf");
+    new->args = ft_get_args(arg);
     new->binpath = ft_get_bin_path(envp);
-    new->pos = 100;
+    new->pos = index - 2;
     new->next = NULL;
     return (new);
+}
+
+void    ft_lstadd_cmd(t_cmd **first, char **envp, char *arg, int index)
+{
+    t_cmd   *newcmd;
+    t_cmd   *tmp;
+
+    if (!envp || !arg)
+        return (NULL);
+    newcmd = ft_newlst(envp, arg, index);
+    tmp = *first;
+    while (tmp && tmp->next)
+        tmp = tmp->next;
+    tmp->next = newcmd;
+}
+
+void    ft_lstiter(t_cmd *first, void (*f)(void *))
+{
+    if (!first)
+        return ;
+    while (first)
+    {
+        f(first);
+        first = first->next;
+    }
 }
 
